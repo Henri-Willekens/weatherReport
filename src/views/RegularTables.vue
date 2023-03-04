674882ca2f86d8610918a9d9553376f381872807
Henri-Willekens <90838600+Henri-Willekens@users.noreply.github.com>
2023-03-04 18:12:00 +0100

    added classes

--- src/views/RegularTables.vue
+++ src/views/RegularTables.vue
@@ -1,105 +1,103 @@
 <template>
-  <div>
-    <base-header class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-success">
-      <!-- Card stats -->
-      <b-row>
-        <b-col xl="3" md="6">
-          <stats-card title="Today:"
-                      type="gradient-red"
-                      class="mb-4">
-                      
+    <div>
+        <base-header class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-success">
+            <!-- Card stats -->
+            <b-row>
+                <b-col xl="3" md="6">
+                    <stats-card title="Today:" type="gradient-red" class="mb-4">
 
-            <template slot="footer">
-              <h3>Today:</h3>
-            <h2>{{ temperature }} <label for="toggle_button">
-                    <span>&#176;{{ isMetric ? "C" : "F" }}</span>
 
-                    <input type="checkbox" id="toggle_button" v-model="checkedValue">
-                </label></h2>
-            <p>{{ weatherDescription }}</p>
+                        <template slot="footer">
+                            <h2>{{ temperature }} <label for="toggle_button">
+                                    <span>&#176;{{ isMetric ? "C" : "F" }}</span>
 
-            <div class="weather-condition">
-                <img v-if="showCloudy" src="../assets/cloudy.png" alt="cloudy">
-                <img v-else-if="showRainy" src="../assets/rain.png" alt="rain">
-                <img v-else-if="showStorm" src="../assets/thunderstorm.png" alt="thunderstorm">
-                <img v-else-if="showClear" src="../assets/clear.png" alt="clear">
-                <img v-else-if="showSnow" src="../assets/snow.png" alt="snow">
-                <img v-else-if="showDrizzle" src="../assets/drizzle.png" alt="drizzle">
-            </div>
+                                    <input type="checkbox" id="toggle_button" v-model="checkedValue">
+                                </label></h2>
+                            <p>{{ weatherDescription }}</p>
 
-            <div class="input-container">
-                <label for="city">City: </label>
-                <input type="text" name="city" v-model="city">
-            </div>
-            </template>
-          </stats-card>
-        </b-col>
-        <b-col xl="3" md="6">
+                            <div class="weather-condition">
+                                <img v-if="showCloudy" src="../assets/cloudy.png" alt="cloudy">
+                                <img v-else-if="showRainy" src="../assets/rain.png" alt="rain">
+                                <img v-else-if="showStorm" src="../assets/thunderstorm.png" alt="thunderstorm">
+                                <img v-else-if="showClear" src="../assets/clear.png" alt="clear">
+                                <img v-else-if="showSnow" src="../assets/snow.png" alt="snow">
+                                <img v-else-if="showDrizzle" src="../assets/drizzle.png" alt="drizzle">
+                            </div>
+
+                            <div class="input-container">
+                                <label for="city">City: </label>
+                                <input type="text" name="city" v-model="city">
+                            </div>
+                        </template>
+                    </stats-card>
+                </b-col>
+                <b-col xl="3" md="6">
                     <table class="weather-condition">
-        <thead>
-            <tr>
-                <th scope="col"></th>
-                <th scope="col">City</th>
-                <th scope="col">Weather today</th>
-                <th scope="col">Temperature today</th>
-                <th scope="col">Weather 24 H</th>
-                <th scope="col">Temperature 24 H</th>
-            </tr>
-        </thead>
-        <tbody>
-            <td scope="row"></td>
-            <td><input readonly type="text" v-model="Forcity" /></td>
-            <td><input readonly type="text" v-model="weatherDescription" /></td>
-            <td><input readonly type="text" v-model="temperature" /></td>
-            <td><input readonly type="text" v-model="ForweatherDescription" /></td>
-            <td><input readonly type="text" v-model="Fortemperature" /></td>
-            <tr v-for="(forecast_data, k) in forecast_datas" :key="k">
-                <button class="trashContainer" type='button' @click="deleteRow(k, forecast_data)"></button>
-                <td><input readonly type="text" v-model="forecast_data.Forcity" /></td>
-                <td><input readonly type="text" v-model="forecast_data.weatherDescription" /></td>
-                <td><input readonly type="text" v-model="forecast_data.temperature" /></td>
-                <td><input readonly type="text" v-model="forecast_data.ForweatherDescription" /></td>
-                <td><input readonly type="text" v-model="forecast_data.Fortemperature" /></td>
-            </tr>
-        </tbody>
-    </table>
+                        <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">City</th>
+                                <th scope="col">Weather today</th>
+                                <th scope="col">Temperature today</th>
+                                <th scope="col">Weather 24 H</th>
+                                <th scope="col">Temperature 24 H</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <td scope="row"></td>
+                            <td><input readonly type="text" v-model="Forcity" /></td>
+                            <td><input readonly type="text" v-model="weatherDescription" /></td>
+                            <td><input readonly type="text" v-model="temperature" /></td>
+                            <td><input readonly type="text" v-model="ForweatherDescription" /></td>
+                            <td><input readonly type="text" v-model="Fortemperature" /></td>
+                            <tr v-for="(forecast_data, k) in forecast_datas" :key="k">
+                                <button class="ni ni-fat-remove" type='button'
+                                    @click="deleteRow(k, forecast_data)"></button>
+                                <td><input readonly type="text" v-model="forecast_data.Forcity" /></td>
+                                <td><input readonly type="text" v-model="forecast_data.weatherDescription" /></td>
+                                <td><input readonly type="text" v-model="forecast_data.temperature" /></td>
+                                <td><input readonly type="text" v-model="forecast_data.ForweatherDescription" /></td>
+                                <td><input readonly type="text" v-model="forecast_data.Fortemperature" /></td>
+                            </tr>
+                        </tbody>
+                    </table>
 
-    <button type='button' @click="addNewRow">Add</button>             
+                    <button type='button' @click="addNewRow">Add</button>
 
 
-            <template slot="footer">
-              <span class="text-success mr-2">54.8%</span>
-              <span class="text-nowrap">Since last month</span>
-            </template>
+                    <template slot="footer">
+                        <span class="text-success mr-2">54.8%</span>
+                        <span class="text-nowrap">Since last month</span>
+                    </template>
 
-        </b-col>
-      </b-row>
-    </base-header>
+                </b-col>
+            </b-row>
+        </base-header>
 
-  </div>
+    </div>
 </template>
 <script>
-  import { Dropdown, DropdownItem, DropdownMenu, Table, TableColumn } from 'element-ui';
-  import projects from './Tables/projects'
-  import users from './Tables/users'
-  import LightTable from "./Tables/RegularTables/LightTable";
-  import DarkTable from "./Tables/RegularTables/DarkTable";
-  import axios from 'axios'
-  export default {
+import { Dropdown, DropdownItem, DropdownMenu, Table, TableColumn } from 'element-ui';
+import projects from './Tables/projects'
+import users from './Tables/users'
+import LightTable from "./Tables/RegularTables/LightTable";
+import DarkTable from "./Tables/RegularTables/DarkTable";
+import axios from 'axios'
+export default {
     components: {
-      LightTable,
-      DarkTable,
-      [Dropdown.name]: Dropdown,
-      [DropdownItem.name]: DropdownItem,
-      [DropdownMenu.name]: DropdownMenu,
-      [Table.name]: Table,
-      [TableColumn.name]: TableColumn
+        LightTable,
+        DarkTable,
+        [Dropdown.name]: Dropdown,
+        [DropdownItem.name]: DropdownItem,
+        [DropdownMenu.name]: DropdownMenu,
+        [Table.name]: Table,
+        [TableColumn.name]: TableColumn
     },
     data() {
-      return {
-        projects,
-        users,
-        showCloudy: false,
+        return {
+            projects,
+            users,
+            showCloudy: false,
             showRainy: false,
             showStorm: false,
             showDrizzle: false,
@@ -113,7 +111,7 @@
             Forcity: '',
             forecast_datas: [{}],
             isMetric: true
-      };
+        };
     },
     computed: {
         checkedValue: {
